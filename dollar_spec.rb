0b18c7ab5c327ddf0dbec87c14850fec1eda8e3d
sinsoku <sinsoku.listy@gmail.com>
2011-02-11 15:04:36 +0900

    specの文章を修正

--- dollar_spec.rb
+++ dollar_spec.rb
@@ -4,8 +4,8 @@ require 'rspec'
 require 'dollar'
 
 describe Dollar do
-  context '$5*2のとき' do
-    it '$10である' do
+  context '金額に数値を掛けるとき' do
+    it '$5*nの金額を得られる' do
       five = Dollar.new(5)
       five.times(2)
       five.amount.should == 10
