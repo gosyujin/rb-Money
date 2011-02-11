63d24ef0bb4d08a19136cdda92fcbe7ca8b488cb
sinsoku <sinsoku.listy@gmail.com>
2011-02-11 13:55:15 +0900

    $5*2=0のspecを追加

---
+++ dollar_spec.rb
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+require 'rubygems'
+require 'rspec'
+
+describe Dollar do
+  context '$5*2のとき' do
+    it '$10である' do
+      five = Dollar.new(5)
+      five.times(2)
+      five.amount.should == 10
+    end
+  end
+end
